10c5bee2b25ce14cfa72658b90cbaab0445fb4e3
coli-geonwoo <148152234+coli-geonwoo@users.noreply.github.com>
2024-04-03 22:09:45 +0900

    [Silver II] Title: 키트 분배하기, Time: 168 ms, Memory: 139568 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/24938. 키트 분배하기/키트 분배하기.py
+++ 백준/Silver/24938. 키트 분배하기/키트 분배하기.py
@@ -1,36 +1,22 @@
+import sys
+input = lambda: sys.stdin.readline().strip('\n')
 n= int(input())
 
 kit = list(map(int, input().split()))
 
 avg = sum(kit)//n
 
-diff =  [avg-i for i in kit]+[0]
+result= 0 
 
-
-result=0
-
-while(True):
-  if len(set(diff))==1 and diff[0]==0:
+for i in range(n):
+  if i==n-1:
     break
+  diff = abs(avg-kit[i])
+  if kit[i] < avg:
+    kit[i+1]-=diff
+  elif kit[i] > avg:
+    kit[i+1]+=diff
   
-  plus_idx=-1
-  minus_idx=-1
-  cnt=0
-  for i in range(n+1):
-    if plus_idx !=-1 and minus_idx != -1:
-      s = min(abs(diff[plus_idx]), abs(diff[minus_idx]))
-      #print(diff[plus_idx], diff[minus_idx])
-      diff[plus_idx] -=s
-      diff[minus_idx]+=s
-      result+= abs(plus_idx-minus_idx)*s
-      break
-
-    if diff[i]>0 and plus_idx==-1:
-      plus_idx = i
-      cnt+=1
-    elif diff[i]<0 and minus_idx==-1:
-      minus_idx= i
-      cnt+=1
-  #print(diff)
-
+  result+=diff
+  #print(kit)
 print(result)

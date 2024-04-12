eb313f76758fd4b4247ee0833858a609c9ac3703
coli-geonwoo <148152234+coli-geonwoo@users.noreply.github.com>
2024-04-13 05:31:25 +0900

    [level 2] Title: 가장 큰 수, Time: 0.02 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,7 +1,20 @@
+from functools import cmp_to_key
 
+'''
 def solution(numbers):
     numbers = [str(i) for i in numbers]
     numbers.sort(key = lambda x: x*4, reverse=True)
     return str(int(''.join(numbers)))
+'''
 
+def compare(x,y):
+    if(str(x)+str(y)<=str(y)+str(x)):
+        return 1
+    else:
+        return -1
+
+def solution(numbers):
+    numbers=[str(i) for i in numbers]
+    numbers.sort(key=cmp_to_key(compare))
+    return str(int(''.join(numbers)))
     
